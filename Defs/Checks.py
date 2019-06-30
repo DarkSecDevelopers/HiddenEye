ba3c45d1d981c72fc5562b0eb5a28819ac996ebd
AnonUD4Y <AnonUD4Y@gmail.com>
2019-06-30 17:29:30 +0530

    Update Checks.py

--- Defs/Checks.py
+++ Defs/Checks.py
@@ -19,8 +19,8 @@ def checkConnection(host='https://google.com'): #Connection check
     try:
         urlopen(host, timeout=5)
         print(_("{0}HURRAY!! Internet is available.. We can Continue{1}").format(GREEN, DEFAULT))
-        print(' Please ZoomIn/ZoomOut Until Below Word Shows in Single Line\n')
-	print('''+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++''')
+        print(" \nPlease ZoomIn/ZoomOut Until Below Word Shows in Single Line\n")
+	print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
         input(" ")
 	return True
     except:
