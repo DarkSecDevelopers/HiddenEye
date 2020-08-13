64b64b706fb287855a57a3001b6ce031bb42ea5b
Restyled.io <commits@restyled.io>
2020-08-13 20:02:52 +0000

    Restyled by autopep8

--- controllers/connection_controller.py
+++ controllers/connection_controller.py
@@ -12,10 +12,8 @@ class ConnectionController:
     def verify_connection(self):
         try:
             if requests.get(url=self._host, timeout=self._timeout).status_code == 200:
-                print(ConnectionView().connection_messages["connection_is_detected"])
+                print(
+                    ConnectionView().connection_messages["connection_is_detected"])
         except:  # HAS TO BE BARE EXCEPT
-            raise ConnectionError(ConnectionView().connection_messages["connection_is_not_detected"]) from None
-
-
-
-
+            raise ConnectionError(
+                ConnectionView().connection_messages["connection_is_not_detected"]) from None
